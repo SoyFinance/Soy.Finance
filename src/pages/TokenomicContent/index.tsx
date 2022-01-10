26e8379765159d1f054483e1ebca8ccdee88323f
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-01-10 10:20:42 -0800

    updated some text

--- src/pages/TokenomicContent/index.tsx
+++ src/pages/TokenomicContent/index.tsx
@@ -80,11 +80,11 @@ const TokenomicContent = () => {
                 </Row2>
                 <Spacer height="10px" />
                 <Row2>
-                    <Text color={Theme.colors.primary}><b>{t('Phase 7')}</b><Text2 color='#767676'>&nbsp;{t('- Harvesting Soy - Details to be published on 10/02/22*.')}</Text2></Text>
+                    <Text color={Theme.colors.primary}><b>{t('Phase 7')}</b><Text2 color='#767676'>&nbsp;{t('- Harvesting Soy - Details to be published on 25/08/22*.')}</Text2></Text>
                 </Row2>
                 <Spacer height="10px" />
                 <Row2>
-                    <Text2 color='#767676'>{t('*Updated on 20/12/21.')}</Text2>
+                    <Text2 color='#767676'>{t('*Updated on 10/01/22.')}</Text2>
                 </Row2>
                 <Spacer height="40px" />
                 <SubTitle color={Theme.colors.bgscondary}>{t('Transaction Fees Distribution')}</SubTitle>
